d57219e411c59378367fa8e6ee8508256007a474
Merith-TK <merith.tk@gmail.com>
2020-05-27 20:36:00 -0700

    functional

--- main.go
+++ main.go
@@ -3,26 +3,45 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"regexp"
 	"time"
 )
 
 func main() {
-	defaultOut, err := exec.Command("usbip", "list", "-l").Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(defaultOut), "\n ------------------------")
-
+	cmd := exec.Command("usbipd", "-D")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Start()
 	for true {
-		newOut, err := exec.Command("usbip", "list", "-l").Output()
+		out, err := exec.Command("usbip", "list", "-l").Output()
 		if err != nil {
 			log.Fatal(err)
 		}
-		if string(defaultOut) != string(newOut) {
-			defaultOut = newOut
-			fmt.Println(string(newOut), "\n ------------------------")
+		usbParse(string(out))
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func usbBind(busid string) {
+	cmd := exec.Command("usbip", "bind", "-b", busid)
+	cmd.Start()
+}
+
+func usbParse(str string) {
+	var re = regexp.MustCompile(`(?m)- busid (.+) \((.+)\)`)
+	var found []string
+	for _, match := range re.FindAllStringSubmatch(str, -1) {
+		found = append(found, match[1])
+	}
+	for i := 0; i < len(found); i++ {
+		busid := found[i]
+		if busid == "1-1.1" || busid == "1-1.2" {
+
+		} else {
+			fmt.Println("BUSID:", busid)
+			usbBind(busid)
 		}
-		time.Sleep(5 * time.Second)
 	}
 }
